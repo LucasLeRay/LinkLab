b2664c589ff2e10dc75f965a8748a7617cb9852b
LucasLeRay <contact@lucas-le-ray.com>
2020-11-20 00:52:12 +0100

    feat: add all possible pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,14 +5,16 @@ import {
   Redirect,
 } from 'react-router-dom'
 import Landing from './pages/Landing'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <Landing />
-        </Route>
+        <Route path="/" exact component={Landing} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
         <Redirect to="/" />
       </Switch>
     </Router>
