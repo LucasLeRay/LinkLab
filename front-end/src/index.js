1418a7591aa3ef62f4cc7c52dda78f58b8108e7a
LucasLeRay <contact@lucas-le-ray.com>
2020-11-21 17:39:57 +0100

    feat: setup AWS amplify

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Amplify } from 'aws-amplify'
+import dotenv from 'dotenv'
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+dotenv.config()
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: process.env.AWS_REGION,
+    userPoolId: process.env.AWS_USER_POOL_ID,
+    identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
+    userPoolWebClientId: process.env.AWS_USER_POOL_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'graphql',
+        endpoint: process.env.AWS_API_GATEWAY_URL,
+        region: process.env.AWS_REGION,
+      },
+    ],
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
