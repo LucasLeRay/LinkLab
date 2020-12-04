64208dcde65458777e10f43dfec248ddf06bbf58
LucasLeRay <lucas.a.leray@gmail.com>
2020-12-04 23:00:56 +0100

    fix: add graphqlEndpoint env variable

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -3,6 +3,7 @@ export default {
   userPoolId: 'us-east-1_LQDDThXwq',
   identityPoolId: 'us-east-1:7524f159-8c8b-49e7-956d-6814ae785b6f',
   userPoolWebClientId: '6im6gq2o14lmrfnm5popfnkrn3',
-  apiGatewayEndpoint:
+  graphqlEndpoint:
     'https://uieucfhu99.execute-api.us-east-1.amazonaws.com/dev/graphql',
+  // 'http://localhost:3001/dev/graphql'
 }
